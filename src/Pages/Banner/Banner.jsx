f30ae02b2bdf535dd661fe061f26be8cbba74211
naymurabir <naymurrahman.abir@gmail.com>
2023-10-07 04:39:49 +0600

    Banner is done with a introducton of our services criterias. And also relavent with the requirements

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -2,11 +2,12 @@
 const Banner = () => {
     return (
 
-        <div className="hero h-[400px]" style={{ backgroundImage: 'url(https://i.ibb.co/6wgHWFZ/Danching.jpg)' }}>
-            <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero h-[400px]" style={{ backgroundImage: 'url(https://i.ibb.co/JtbVjdM/Concert.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60 "></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-2xl">
-                    <h1 className="mb-5 text-white text-3xl font-normal">Unforgettable Moments, Exceptional Events. Discover the Magic of <span className="text-[#AC915A]">Party Perfection</span></h1>
+                    <h1 className="mb-5 text-white text-3xl font-normal">Unforgettable Moments, Exceptional Events. Discover the Magic of <span className="text-orange-500 font-bold text-5xl">Party Perfection</span></h1>
+
                 </div>
             </div>
         </div>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,34 +1,37 @@
 import { NavLink } from "react-router-dom";
 
+
 const Navbar = () => {
 
     const navLinks = <>
-        <NavLink className="text-lg font-semibold mr-3" to="/">Home</NavLink>
+        <NavLink className="text-lg font-bold mr-4 text-white" to="/">Home</NavLink>
 
-        <NavLink className="text-lg font-semibold" to="/">Services</NavLink>
+        <NavLink className="text-lg font-bold text-white" to="/">Services</NavLink>
     </>
 
     return (
-
-        <div className="navbar">
-            <div className="navbar-start">
+        <div className="navbar justify-between fixed md:w-[88%] lg:w-[80%] z-10 px-8">
+            <div className="flex justify-between">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  bg-black rounded w-32">
                         {navLinks}
                     </ul>
                 </div>
-                <img className="w-40 hidden lg:block" src="https://i.ibb.co/P5nbpJF/Party-Perfection.gif" alt="" />
+
+                <h2 className="text-2xl text-white font-semibold hidden lg:block mr-">Party Perfection</h2>
+                <img className="w-16 h-[60px] hidden lg:block" src="https://i.ibb.co/w7QNwZT/Logo.png" alt="" />
             </div>
-            <div className="navbar-center hidden lg:flex">
+
+            <div className="navbar-center hidden  mr-20 lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {navLinks}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <button className="bg-rose-600 text-white px-4 py-2 rounded font-semibold">Log Out</button>
+            <div className="">
+                <button className="bg-orange-400 text-white px-4 py-1 rounded font-semibold">Log In</button>
             </div>
         </div>
     );
