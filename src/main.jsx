5f765b1266df34856cd15b3555b3ffde69a07b5a
naymurabir <naymurrahman.abir@gmail.com>
2023-10-08 09:12:39 +0600

    Email and Password field validation complted

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import router from './Routes/Routes.jsx'
 import AuthProvider from './Providers/AuthProvider/AuthProvider'
 
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
